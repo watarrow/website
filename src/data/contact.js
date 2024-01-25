85480eb1b0171f2ca51b3be9f5a75b7b0a373e8d
Thomas Kim <thomasjuhoonkim@gmail.com>
2024-01-24 23:53:17 -0500

    Add contact page

---
+++ src/data/contact.js
@@ -0,0 +1,24 @@
+const contacts = [
+  {
+    purpose: "General Inquiries",
+    name: "WatArrow General Email",
+    email: "waterlooarrow@gmail.com",
+  },
+  {
+    purpose: "Team Inquiries",
+    name: "Emma-Lee Keeping | Team Captain",
+    email: "emma.keeping@uwaterloo.ca",
+  },
+  {
+    purpose: "Business Inquiries",
+    name: "Thomas Kim | Business Director",
+    email: "thomas.kim@uwaterloo.ca",
+  },
+  {
+    purpose: "Engineering Inquiries",
+    name: "Joshua Perry | Technical Director",
+    email: "j9perry@uwaterloo.ca",
+  },
+];
+
+export default contacts;
--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,6 +1,39 @@
-import InDevelopment from "@/components/InDevelopment";
+import FadeIn from "react-fade-in";
+import Link from "next/link";
+
+import Arrow from "@/assets/logo-transparent-svg.svg";
+
+import contacts from "@/data/contact";
 
 const Contact = () => {
-  return <InDevelopment />;
+  return (
+    <main className="contact-root">
+      <div className="contact-container">
+        <div className="left">
+          <div className="header">
+            <h1>CONTACT US</h1>
+            <p>
+              If you are interested in working with us, sponsoring us, or want
+              to learn more, feel free to reach out to us!
+            </p>
+          </div>
+          {contacts.map((contact, i) => (
+            <div className="contact-card" key={i}>
+              <h2>{contact.purpose}</h2>
+              <p>{contact.name}</p>
+              <Link className="email" href={`mailto:${contact.email}`}>
+                {contact.email}
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div className="right">
+          <div className="logo">
+            <Arrow />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 export default Contact;
