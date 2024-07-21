f28e5d7221e5899255f026e13638bd4ddcc192ec
Thomas Kim <thomasjuhoonkim@gmail.com>
2024-07-21 15:07:14 -0400

    Add business role

--- src/data/roles.js
+++ src/data/roles.js
@@ -11,18 +11,11 @@ const roles = [
   //   href: "",
   //   visible: true,
   // },
-  // {
-  //   title: "Business Team - General Member",
-  //   description: "",
-  //   href: "",
-  //   visible: true,
-  // },
-  // {
-  //   title: "Engineering Team - Flight Controls Subteam Member",
-  //   description: "",
-  //   href: "",
-  //   visible: true,
-  // },
+  {
+    title: "Business Team - General Member",
+    href: "https://forms.gle/oL71HhLXA2TvY7As5",
+    visible: true,
+  },
 ];
 
 export default roles;
