aa33a9d5ec5744904ee3fe88ae04890fdebaf553
Thomas Kim <thomasjuhoonkim@gmail.com>
2026-07-19 12:54:26 -0400

    THis hsould build

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
+      rule.test?.test?.(".svg"),
     );
 
     config.module.rules.push(
@@ -26,7 +26,7 @@ module.exports = {
             options: { icon: true, dimensions: false },
           },
         ],
-      }
+      },
     );
 
     // Modify the file loader rule to ignore *.svg, since we have it handled now.
